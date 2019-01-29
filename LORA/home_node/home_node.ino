384bc6164806cf07c52e269fe536c7c21db1644d
= <rdwivedi345@gmail.com  git config --global user.name Your>
2019-01-29 17:49:00 +0530

    HOME NODE REPAIRED

--- LORA/home_node/home_node.ino
+++ LORA/home_node/home_node.ino
@@ -1,16 +1,8 @@
-/*
- * work to be DONE: 
- * 1) reciver code for checking power shed mode
- * 
-*/
-
-
 #include <SPI.h>
 #include "LoRa.h"
 #include<Arduino.h>
 
 // WIFI_LoRa_32 ports
-
 // GPIO5  -- SX1278's SCK
 // GPIO19 -- SX1278's MISO
 // GPIO27 -- SX1278's MOSI
@@ -23,93 +15,131 @@
 #define DI0     26
 #define BAND    433E6  //915E6 --
 
+ 
 //---------------------------GLOBAL VARIABLES (ps:PowerShed,n:normal)--------------------------------------------------
-
-int threshold = 10;   // change to 3200 of as per meter (no of led blinks per unit)
+int UniqueDeviceId = 1001;
+int threshold = 5;   // change to 3200 of as per meter (no of led blinks per unit)
 int ps_Blinks=0;
 int n_Blinks=0;
 int ps_Mode=0;
 int n_UnitsConsumed=0;
 int ps_UnitsConsumed=0;
+int msgID;
+unsigned long dayStart=0;
+unsigned long fullDay = 120000;    // assuming day to be of 2 min ie 120000 ms
 
+//----packet variables-----------------
+
+int ps_Timestamp=0;
+int ps_time=0;
+int ps_unit=0;
+//--------pin variables
+int blinkPin = 16; // pin to read blinks
+int relayPin = 17; // pin to control relay
+ 
 //------------------------- SETUP ------------------------------------------------------
+
 void setup() {
-  pinMode(25,OUTPUT); //Send success, LED will bright 1 second
-  pinMode(33,INPUT); //Send success, LED will bright 1 second
-  
+  pinMode(blinkPin,INPUT); // READS BLINK FROM METER
+  pinMode(relayPin,OUTPUT); // contorl RELAY pin
+  digitalWrite(relayPin,HIGH); // turn relay on
   Serial.begin(115200);
   while (!Serial); //If just the the basic function, must connect to a computer
-  
-  SPI.begin(5,19,27,18);
+//  SPI.begin(5,19,27,18);
+  SPI.begin();
   LoRa.setPins(SS,RST,DI0);
-//  Serial.println("LoRa Sender");
 
   if (!LoRa.begin(BAND)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
   Serial.println("LoRa Initial OK!");
+  dayStart = millis();
 }
+
 //---------------------------   LOOP --------------------------------------
 void loop() {
+  
+  myRecieve();
+  checkPsMode();
+  countIncrement();
+  trip();
+  dayOff();
+ }
 
-countIncrement();
+//--------------------------my functions ----------------------------------
+//-------------- FUNCTION TO SEND DATA ---------------------------------------
 
+void dayOff()   // function to calculate day's end and send data to rpi
+{
+if(millis()-dayStart >= fullDay){
+  sendToPi();
+  dayStart=millis();
+  }  
 }
 
 
-//--------------------------my functions ----------------------------------
+// sendToPi method sends data about the consumption of data to rpi using lora
+void sendToPi(){
+  char rpiData[30];
+  sprintf(rpiData,"%d:%d",UniqueDeviceId,( n_UnitsConsumed + ps_UnitsConsumed));
+  LoRa.beginPacket();
+  LoRa.print(rpiData);
+  LoRa.endPacket();
+}
 
-//-------------- FUNCTION TO SEND DATA ---------------------------------------
+void trip(){
+if(ps_Mode == 1 && ps_UnitsConsumed >= ps_unit ){
+  digitalWrite(relayPin,LOW);
+  }else{
+   digitalWrite(relayPin,HIGH); 
+    }
+}
 
-<<<<<<< HEAD
 void mySend()
 {
   LoRa.beginPacket();
-  LoRa.print("NODE2: ");
-  LoRa.print(counter);
+  //send consumption if time passed is 24h
+  LoRa.print("");
   LoRa.endPacket();
-  Serial.println("DATA SEND: ");
-  Serial.println(counter);
 }
 
-=======
->>>>>>> e07ae63192b0979c2352df763f0acd3dbf3bc56d
 //---------------- FUNCTION TO SET INTO RECIEVE MODE -------------------------------------
 void myRecieve()
-{
+{ 
   int packetSize = LoRa.parsePacket();
   if (packetSize){
-    // received a packet
-    Serial.print("Received'");
-
+    String str1;
+  
     // read packet
     while (LoRa.available())
     {
-      Serial.print((char)LoRa.read());
+     str1.concat((char)LoRa.read());
     }
-<<<<<<< HEAD
-
-    // print RSSI of packet
-    Serial.print("' with RSSI ");
-    Serial.println(LoRa.packetRssi());
-  }
-}
-
-
-void checkPsMode(){}
-=======
     char *str= &str1[0];
     msgID = atoi(strtok(str , ":" ));
     ps_time = 1000 * atoi(strtok(0 , ":" ));
     ps_unit = atoi(strtok(0 , ":" ));
     ps_Timestamp = millis();
-    ps_Mode = 1;
+    //ps_Mode = 1;
     Serial.println("in ps mode");
     delay(2000);
   }
 }
 
+
+void checkPsMode(){
+  if(msgID!=0 ){
+  ps_Mode=1;
+  Serial.println(ps_Mode);
+  }
+  else{
+    ps_Mode=0;
+    Serial.println(ps_Mode);
+    }
+}
+
+//------------------------------------ RESETS THE PSMOD AND ALL PS VARIABLES WHEN TIMOUT ------------------------
 void psReset(){   
   if( millis() - ps_Timestamp >= ps_time ){
     msgID = 0;
@@ -117,48 +147,29 @@ void psReset(){
     ps_unit = 0;
     ps_Timestamp = 0;
     ps_Mode = 0;
-    n_UnitsConsumed = n_UnitsConsumed + ps_UnitsConsumed;
-    ps_UnitsConsumed=0 ;   
     }
 }
->>>>>>> e07ae63192b0979c2352df763f0acd3dbf3bc56d
-
-void checkPsMode(){
-  if(msgID!=0 ){ //check if home node is in Power shed mode?
-    psReset();
-  }
-}
-
-//------------------------------------ RESETS THE PSMOD AND ALL PS VARIABLES WHEN TIMOUT ------------------------
-
 
 void countIncrement(){
+  Serial.println("in counter increment");
   if(ps_Mode==1)
-  {
+  { 
     psModeConsume();  // check for no blink and units consumption
+    psReset();
   }
     else if(ps_Mode==0){
-
-    nModeConsume();
+      nModeConsume();
       }
 }
 
 void psModeConsume(){
-<<<<<<< HEAD
-  if(digitalRead(33)==1)
-      {
-        while(digitalRead(33)==1) // check debouncing
-=======
  if(digitalRead(blinkPin)){
         Serial.println();
         Serial.print("ps BLINK INCREMENTED:");
-        delay(100);
->>>>>>> e07ae63192b0979c2352df763f0acd3dbf3bc56d
+        delay(1000);
         ++ps_Blinks;
       }
-
-    
-    if(n_Blinks >= threshold){
+    if(ps_Blinks >= threshold){
       ps_UnitsConsumed++;
       ps_Blinks=0;
       sendData();
@@ -167,34 +178,28 @@ void psModeConsume(){
 }
 
 void nModeConsume(){
-  if(digitalRead(33)==1)
+  
+  
+  Serial.println();
+  Serial.print("blinkPin pin state in nmode:");
+   if(digitalRead(blinkPin))
       {
-<<<<<<< HEAD
-        while(digitalRead(33)==1) // check debouncing
-        ++n_Blinks;
-=======
         Serial.print("n BLINK INCREMENTED");
         
-        delay(100);
+        delay(1000);
          ++n_Blinks;
->>>>>>> e07ae63192b0979c2352df763f0acd3dbf3bc56d
       } 
+
       if(n_Blinks >= threshold){
-      n_UnitsConsumed++;
-      n_Blinks=0;
-      sendData();
+        n_UnitsConsumed++;
+        n_Blinks=0;
+        sendData();
     }
 }
 
 void sendData(){
-<<<<<<< HEAD
-  LoRa.print("\nconsumption NORMAL:");LoRa.print(n_UnitsConsumed);
-  LoRa.print("\n PowerShed:");LoRa.print(ps_UnitsConsumed);
-  Serial.print("data Sent :)");
-=======
   char myData[40];
   sprintf(myData,"consumption NORMAL: %d PowerShed:%d ",n_UnitsConsumed,ps_UnitsConsumed);
   Serial.println(myData);
-  delay(1000);
->>>>>>> e07ae63192b0979c2352df763f0acd3dbf3bc56d
+  delay(2000);
 }
